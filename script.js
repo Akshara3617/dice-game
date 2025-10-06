ae3889e44c7999c8e8255b0ab91b82f3b296e1d6
Akshara3617 <162999887+Akshara3617@users.noreply.github.com>
2025-10-06 14:13:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let button = document.getElementById('rollBtn')
+let image1 = document.querySelector('.img1')
+let image2 = document.querySelector('.img2')
+let heading = document.querySelector('h1')
+
+button.addEventListener('click', function(){
+   let randomNumber1 = Math.floor(Math.random() * 6) + 1
+   let randomNumber2 = Math.floor(Math.random() * 6) + 1
+   image1.setAttribute('src',`images/dice${randomNumber1}.png`)
+   image2.setAttribute('src',`images/dice${randomNumber2}.png`)
+
+   if(randomNumber1 > randomNumber2){
+      heading.textContent = 'Player 1 Wins'
+   } else if(randomNumber1 < randomNumber2){
+      heading.textContent = 'Player 2 Wins'
+   } else {
+      heading.textContent = 'Draw'
+   }
+})
